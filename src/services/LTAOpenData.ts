20a6e2dab55b129262833499b0a242d4a1dac6fb
Jerald Yik <jeraldyik12@gmail.com>
2020-12-10 17:09:16 +0800

    added bus stop name/road as input

--- src/services/LTAOpenData.ts
+++ src/services/LTAOpenData.ts
@@ -6,27 +6,51 @@ import moment from 'moment';
  */
 
 const MAX_NEXTBUS = 2;
+
+interface IHeaders {
+  'AccountKey': string
+}
 interface IParsedData {
   bus: string,
   nextArrival: Array<string>;
 }
 
 const LTAOpenDataService = (bot) => {
-  const apiKey = process.env.LTADATAMALL_APIKEY;
-  const _headers = {'AccountKey' : apiKey};
+  const apiKey: string = process.env.LTADATAMALL_APIKEY ? process.env.LTADATAMALL_APIKEY : '';
+  const _headers: IHeaders = {'AccountKey' : apiKey};
+
+  const GREEN_CIRCLE = '\uD83D\uDFE2'; 
+  const ORANGE_CIRCLE = '\uD83D\uDFE0';
+  const RED_CIRCLE = '\uD83D\uDD34';
 
   bot.onText(/\/bus (.+)/, async (msg, match): Promise<void> => {  
     const busStop = match[1];
     const chatId = msg.chat.id;
-    const uri = `http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=${busStop}`;
+    let foundBusStop;
+    if (!/^\d{5}$/.test(busStop)) {
+      foundBusStop = await fromLandmarkNameToBusStopCode(busStop, _headers);
+      if (foundBusStop === '00000') {
+        bot.sendMessage(chatId, 'Bus Services ceased to service the input bus stop/road for today');
+        return;
+      } else if (!/^\d{5}$/.test(foundBusStop)) {
+        // error
+        bot.sendMessage(chatId, 'There is an error! Please try again.\n' + foundBusStop);
+        return;
+      }
+    }
+    
+    const uri = `http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=${foundBusStop ? foundBusStop : busStop}`;
     
     try {
       const response = await axios.get(uri, {headers: _headers});
-      bot.sendMessage(chatId, `Querying for Bus Stop #${response.data.BusStopCode}`);
       if (response.status === 200) {    
         if (response.data.Services.length > 0) {
           const parsedData = parseData(response.data.Services);
-          let s = `Bus Stop #${response.data.BusStopCode}\n---------------------------\n`;
+          let s = `Bus Stop #${foundBusStop ? `${foundBusStop} (${busStop})` : busStop}\n`;
+          s += GREEN_CIRCLE + ' Seats Available\n';
+          s += ORANGE_CIRCLE + ' Standing Available\n';
+          s += RED_CIRCLE + ' Limited Standing\n';
+          s += '---------------------------\n'
           parsedData.forEach(d => {
             s += `Bus ${d.bus}:\n`;
             d.nextArrival.forEach(n => {
@@ -45,28 +69,35 @@ const LTAOpenDataService = (bot) => {
     }
   });
 
-  bot.onText(/\/traindown/, async (msg): Promise<void> => {  
-    const chatId = msg.chat.id;
-    const uri = `http://datamall2.mytransport.sg/ltaodataservice/TrainServiceAlerts`;
+  const fromLandmarkNameToBusStopCode = async (landmark: string, _headers: IHeaders): Promise<string> => {
+    const uri = `http://datamall2.mytransport.sg/ltaodataservice/BusStops`;
+    landmark = landmark.replace(/[ ,.']/g, "").toLowerCase();
+
     try {
       const response = await axios.get(uri, {headers: _headers});
       if (response.status === 200) {  
-        if (response.data.value.Status === 1) {
-          bot.sendMessage(chatId, 'No Train Disruption');
-        } else {
-          bot.sendMessage(chatId, 'Please take note...\n' + JSON.stringify(response.data.value));
-        }
-      } else {
-        bot.sendMessage(chatId, 'There is an Error!\nReponse Code: ' + response.status.toString());
-      }
-  
+        if (response.data.value.length > 0) {
+          for (var o of response.data.value) {
+            if (o.Description.replace(/[ ,.']/g, "").toLowerCase() === landmark) {
+              return o.BusStopCode;
+            }
+          };
+          // resort to finding by street name
+          for (var o of response.data.value) {
+            if (o.RoadName.replace(/[ ,.']/g, "").toLowerCase() === landmark) {
+              return o.BusStopCode;
+            }
+          };
+        } 
+      } 
     } catch (err) {
-      bot.sendMessage(chatId, 'There is an error! Please try again.\n' + err.toString());
+      return err.toString();
     }
+    // let main function handle errors
+    return '00000';
+  };
 
-  });
-
-  const parseData = (data): Array<IParsedData> => {
+  const parseData = (data: Array<any>): Array<IParsedData> => {
     const returnArr: Array<IParsedData> = [];
     data.forEach(d => {
       const aux: IParsedData = {} as IParsedData;
@@ -90,22 +121,40 @@ const LTAOpenDataService = (bot) => {
     });
     return returnArr;
   }
-}
 
-const seatsAvailability = (code: string): string => {
-  const GREEN_CIRCLE = '\uD83D\uDFE2'; 
-  const ORANGE_CIRCLE = '\uD83D\uDFE0';
-  const RED_CIRCLE = '\uD83D\uDD34';
-  switch (code) {
-    case 'SEA':
-      return GREEN_CIRCLE;
-    case 'SDA':
-      return ORANGE_CIRCLE;
-    case 'LSD':
-      return RED_CIRCLE;
-    default: 
-      return '';
+  const seatsAvailability = (code: string): string => {
+    switch (code) {
+      case 'SEA':
+        return GREEN_CIRCLE;
+      case 'SDA':
+        return ORANGE_CIRCLE;
+      case 'LSD':
+        return RED_CIRCLE;
+      default: 
+        return '-';
+    }
   }
+
+  bot.onText(/\/traindown/, async (msg): Promise<void> => {  
+    const chatId = msg.chat.id;
+    const uri = `http://datamall2.mytransport.sg/ltaodataservice/TrainServiceAlerts`;
+    try {
+      const response = await axios.get(uri, {headers: _headers});
+      if (response.status === 200) {  
+        if (response.data.value.Status === 1) {
+          bot.sendMessage(chatId, 'No Train Disruption');
+        } else {
+          bot.sendMessage(chatId, 'Please take note...\n' + JSON.stringify(response.data.value));
+        }
+      } else {
+        bot.sendMessage(chatId, 'There is an Error!\nReponse Code: ' + response.status.toString());
+      }
+  
+    } catch (err) {
+      bot.sendMessage(chatId, 'There is an error! Please try again.\n' + err.toString());
+    }
+
+  });
 }
 
 export default LTAOpenDataService;
